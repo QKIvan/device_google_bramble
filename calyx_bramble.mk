e32382d170bb1d15d4aa4fc2932641abbc12911c
Chirayu Desai <chirayudesai1@gmail.com>
2019-03-23 17:29:23 +0530

    Add initial Calyx product makefiles
    Also includes:
    commit 3ad5944c02f34fc4c0e74f71d48d80a0e9c16fba
    Author: Chirayu Desai <chirayudesai1@gmail.com>
    Date:   Thu Nov 12 03:16:43 2020 +0530
    
        calyx: Inherit device-calyx.mk
    
        Change-Id: Ic73a5b0f4108f0f27ed615cc8f2ab2367cb73637
        (cherry picked from commit 1f3697ca4bc2c7082c0447b3801f644107b91691)
    
    Change-Id: I0781b6d4d74680ce7f0d860048871da7c2d43fb6
    (cherry picked from commit d4bfbf38f99722c3628764423dc822cc23e613be)

---
+++ calyx_bramble.mk
@@ -0,0 +1,19 @@
+# Inherit some common stuff.
+$(call inherit-product, vendor/calyx/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/redbull/device-calyx.mk)
+$(call inherit-product, device/google/bramble/aosp_bramble.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := calyx_bramble
+PRODUCT_MODEL := Pixel 4a (5G)
+PRODUCT_BRAND := google
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=bramble \
+    PRIVATE_BUILD_DESC="bramble-user 13 TP1A.220624.014 8819323 release-keys"
+
+BUILD_FINGERPRINT := google/bramble/bramble:13/TP1A.220624.014/8819323:user/release-keys
+
+PRODUCT_RESTRICT_VENDOR_FILES := false
