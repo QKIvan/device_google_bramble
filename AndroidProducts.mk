b162b623cdda36185f052c4577a7c44f593ee27b
Chirayu Desai <chirayudesai1@gmail.com>
2019-03-23 17:29:23 +0530

    Add initial Calyx product makefiles
    Also includes:
    commit 3ad5944c02f34fc4c0e74f71d48d80a0e9c16fba
    Author: Chirayu Desai <chirayudesai1@gmail.com>
    Date:   Thu Nov 12 03:16:43 2020 +0530
    
        calyx: Inherit device-calyx.mk
    
        Change-Id: Ic73a5b0f4108f0f27ed615cc8f2ab2367cb73637
        (cherry picked from commit 1f3697ca4bc2c7082c0447b3801f644107b91691)
    
    Change-Id: I0781b6d4d74680ce7f0d860048871da7c2d43fb6
    (cherry picked from commit d4bfbf38f99722c3628764423dc822cc23e613be)

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -18,6 +18,8 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_bramble.mk \
     $(LOCAL_DIR)/aosp_bramble_64.mk \
     $(LOCAL_DIR)/aosp_bramble_hwasan.mk \
+    $(LOCAL_DIR)/calyx_bramble.mk \
 
 COMMON_LUNCH_CHOICES := \
     aosp_bramble-userdebug \
+    calyx_bramble-userdebug \
