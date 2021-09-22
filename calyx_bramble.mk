1838af4a102161f035b5e31f5158523c41dbc75f
Michael Bestas <mkbestas@gmail.com>
2021-09-23 00:10:56 +0300

    calyx: Override product name/fingerprint/description
    Change-Id: Ie673890e0380a1b88ecb010a085e4d3c3fff9c61
    (cherry picked from commit f7fa46ca681e665575b5f075479e00c75f825329)

--- calyx_bramble.mk
+++ calyx_bramble.mk
@@ -11,6 +11,12 @@ PRODUCT_NAME := calyx_bramble
 PRODUCT_MODEL := Pixel 4a (5G)
 PRODUCT_BRAND := google
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=bramble \
+    PRIVATE_BUILD_DESC="bramble-user 12 SP2A.220305.012 8177914 release-keys"
+
+BUILD_FINGERPRINT := google/bramble/bramble:12/SP2A.220305.012/8177914:user/release-keys
+
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 $(call inherit-product, vendor/google/bramble/bramble-vendor.mk)
