398398ccaa1c653c1a1822b582a24d6eab78c5b1
Michael Bestas <mkbestas@gmail.com>
2023-01-04 03:59:05 +0200

    Update fingerprint/build description from TQ1A.230105.001
    Change-Id: I63898908c494b360c17839a7c00b8390e31451b0

--- calyx_bramble.mk
+++ calyx_bramble.mk
@@ -13,9 +13,9 @@ PRODUCT_BRAND := google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=bramble \
-    PRIVATE_BUILD_DESC="bramble-user 13 TQ1A.221205.011 9244662 release-keys"
+    PRIVATE_BUILD_DESC="bramble-user 13 TQ1A.230105.001 9292298 release-keys"
 
-BUILD_FINGERPRINT := google/bramble/bramble:13/TQ1A.221205.011/9244662:user/release-keys
+BUILD_FINGERPRINT := google/bramble/bramble:13/TQ1A.230105.001/9292298:user/release-keys
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
