baade153efe006bb3ae208fcdc89792f105bf52c
Chirayu Desai <chirayudesai1@gmail.com>
2023-11-07 14:32:11 +0530

    Update fingerprint/build description from UP1A.231105.001
    Change-Id: I3b8bee02b7472685943d3bc1c9ff7c06129819a5

--- calyx_bramble.mk
+++ calyx_bramble.mk
@@ -13,9 +13,9 @@ PRODUCT_BRAND := google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=bramble \
-    PRIVATE_BUILD_DESC="bramble-user 14 UP1A.231005.007 10754064 release-keys"
+    PRIVATE_BUILD_DESC="bramble-user 14 UP1A.231105.001 10817346 release-keys"
 
-BUILD_FINGERPRINT := google/bramble/bramble:14/UP1A.231005.007/10754064:user/release-keys
+BUILD_FINGERPRINT := google/bramble/bramble:14/UP1A.231105.001/10817346:user/release-keys
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
